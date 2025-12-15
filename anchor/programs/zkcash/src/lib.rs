aa60ccff24e202ad7aac8147942da93ac14833d8
Zhe Wang <zhew1991@gmail.com>
2025-12-15 14:11:47 -0500

    add new tokens to devnet

--- anchor/programs/zkcash/src/lib.rs
+++ anchor/programs/zkcash/src/lib.rs
@@ -38,7 +38,12 @@ pub const ALLOW_ALL_SPL_TOKENS: bool = true;
 pub const ALLOW_ALL_SPL_TOKENS: bool = false;
 
 #[cfg(feature = "devnet")]
-pub const ALLOWED_TOKENS: &[Pubkey] = &[pubkey!("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU")];
+pub const ALLOWED_TOKENS: &[Pubkey] = &[
+    pubkey!("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"), // USDC
+    pubkey!("EcFc2cMyZxaKBkFK1XooxiyDyCPneLXiMwSJiVY6eTad"), // USDT
+    pubkey!("6zxkY8UygHKBf64LJDXnzcYr9wdvyqScmj7oGPBFw58Z"), // ORE
+    pubkey!("Vu3Lcx3chdCHmy9KCCdd19DdJsLejHAZxm1E1bTgE16") // ZEC
+];
 
 #[cfg(not(feature = "devnet"))]
 pub const ALLOWED_TOKENS: &[Pubkey] = &[
